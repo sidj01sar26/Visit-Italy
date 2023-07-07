9b7889c797aaf75bada56b9342b2f8a89ccc69b0
Siddharth Joisar <127229870+sidj01sar26@users.noreply.github.com>
2023-07-07 21:34:47 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,93 @@
+const data = [
+	{
+		id: 1,
+		name: "Rome",
+
+		info: "Rome is the capital city of Italy. It is also the capital and largest city in the region of Lazio. It is on the Tiber River. When the Roman Empire was new, Rome was called the Eternal City. Rome is well known for being the home of the Catholic Church, including its leader, the Pope. He lives in the Vatican City, which is an enclave in the north-west part of Rome.",
+
+		image: "https://res.klook.com/image/upload/Mobile/City/afmqgg5h0jl9wnr1dfmf.jpg",
+		price: "3,200",
+	},
+
+	{
+		id: 2,
+		name: "Venice",
+
+		info: "Venice Italy is an Italian city located in the northeast of the State. Venice, Venezia in Italian, is best known for its beauty, canals, architecture, and art. But there is more! This small city founded in the middle of a lagoon has been one the most powerful European States for several centuries, giving birth to a culture and lifestyle that are incredibly unique.",
+
+		image: "https://www.wanderlustchloe.com/wp-content/uploads/2019/03/day-trips-from-venice-11.jpg",
+		price: "2,800",
+	},
+
+	{
+		id: 3,
+		name: "Milan",
+
+		info: "Milan is the capital city of the region of Lombardy in northern Italy. Milan is known as Italy's “moral capital.” Milanese believe that their positive work ethic has led to Milan becoming a world capital of fashion, design, finance, business services, and media and publishing. Milan is also famous for its art and architecture, as it is the home of Leonardo da Vinci",
+
+		image: "https://fullsuitcase.com/wp-content/uploads/2023/03/Best-things-to-do-in-Milan-Italy.jpg",
+		price: "2,500",
+	},
+
+	{
+		id: 4,
+		name: "Florence",
+
+		info: "Florence, capital of Italy's Tuscany region, is home to many masterpieces of Renaissance art and architecture. One of its most iconic sights is the Duomo, a cathedral with terracotta-tiled dome engineered by Brunelleschi and a bell tower by Giotto. The Galleria dell'Accademia displays Michelangelo's “David” sculpture. The Uffizi Gallery exhibits Botticelli's The Birth of Venus” and da Vinci's Annunciation.",
+
+		image: "https://media.timeout.com/images/105879414/750/422/image.jpg",
+		price: "2,250",
+	},
+
+	{
+		id: 5,
+		name: "Naples",
+
+		info: "Naples, Italian Napoli, ancient (Latin) Neapolis (“New Town”), city, capital of Naples provincia, Campania regione, southern Italy. It lies on the west coast of the Italian peninsula, 120 miles (190 km) southeast of Rome. On its celebrated bay—flanked to the west by the smaller Gulf of Pozzuoli and to the southeast by the more extended indentation of the Gulf of Salerno.",
+
+		image: "https://fullsuitcase.com/wp-content/uploads/2022/05/Naples-day-trips-and-day-tours-from-Napoli-Italy.jpg.webp",
+		price: "2,100",
+	},
+
+	{
+		id: 6,
+		name: "Lake Como",
+
+		info: "Lake Como, in Northern Italy's Lombardy region, is an upscale resort area known for its dramatic scenery, set against the foothills of the Alps. The lake is shaped like an upside-down Y, with three slender branches that meet at the resort town of Bellagio. At the bottom of the southwest branch lies the city of Como, home to Renaissance architecture and a funicular that travels up to the mountain town of Brunate.It is one of the most popular tourist attractions in the world",
+
+		image: "https://d3dqioy2sca31t.cloudfront.net/Projects/cms/production/000/024/621/medium/588dd532cfeaa78befa7c97903753ac3/italy-lake-como-villa-del-balbianello-100418-ch.jpg",
+		price: "1,800",
+	},
+
+	{
+		id: 7,
+		name: "Palermo",
+
+		info: "Palermo is the capital of the Italian island of Sicily. The 12th-century Palermo Cathedral houses royal tombs, while the huge neoclassical Teatro Massimo is known for opera performances. Also in the center are the Palazzo dei Normanni, a royal palace started in the 9th century, and the Cappella Palatina, with Byzantine mosaics. Busy markets include the central Ballarò street market and the Vucciria, near the port.",
+
+		image: "https://www.italia.it/content/dam/tdh/en/interests/sicilia/palermo/palermo/media/20210401173355-cattedrale-di-palermo-sicilia-gettyimages-691244576-1.jpg",
+		price: "1,500",
+	},
+
+	{
+		id: 8,
+		name: "Verona",
+
+		info: "Verona is a city in northern Italy's Veneto region, with a medieval old town built between the meandering Adige River. It's famous for being the setting of Shakespeare's Romeo and Juliet. A 14th-century residence with a tiny balcony overlooking a courtyard is said be “Juliet's House. The Verona Arena is a huge 1st-century Roman amphitheater, which currently hosts concerts and large-scale opera performances.",
+
+		image: "https://www.centralholidays.com/MyUpload/020420201527251Verona850x530_2.jpg",
+		price: "1,200",
+	},
+
+	{
+		id: 9,
+		name: "Turin",
+		
+		info: "Turin is the capital city of Piedmont in northern Italy, known for its refined architecture and cuisine. The Alps rise to the northwest of the city. Stately baroque buildings and old cafes line Turin's boulevards and grand squares such as Piazza Castello and Piazza San Carlo. Nearby is the soaring spire of the Mole Antonelliana, a 19th-century tower housing the interactive National Cinema Museum.",
+
+		image: "https://tourismmedia.italia.it/is/image/mitur/20220223132855-piazza-san-carlo-torino-piemonte-shutterstock-2102981095-rid-1?wid=1080&hei=660&fit=constrain,1&fmt=webp",
+		price: "1,100",
+	},
+];
+
+export default data;
